48d395cd84594002a54293c7f85362738bf2ce41
alisa <alisa@Otter.localdomain>
2022-09-08 15:35:24 +0300

    homework point 1

--- fith-app/src/pages/HomePage.js
+++ fith-app/src/pages/HomePage.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
     const [data, setData] = useState({});
+    console.log(data);
 
     useEffect(() => {
         db.child('contacts').on('value', (snap) => {
@@ -21,6 +22,17 @@ const HomePage = () => {
         }
     }, [])
 
+    const deleted = (event) => {
+        console.log(event.target.id);
+        db.child(`contacts/${event.target.id}`).remove((err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Deleted");
+            }
+          });
+    }
+
     return (
         <div>
             <table>
@@ -34,10 +46,11 @@ const HomePage = () => {
                 <tbody>
                     {Object.keys(data).map((id) => {
                         return (
-                            <tr key={data[id]}>
+                            <tr key={id}>
                                 <td><Link to={'/aboutcontact/' + data[id].name}>{data[id].name}</Link></td>
                                 <td>{data[id].email}</td>
                                 <td>{data[id].contact}</td>
+                                <td onClick={deleted} id={id}>Delete</td>
                             </tr>
                         )
                     })}
